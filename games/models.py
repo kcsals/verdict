21b5fe931a99a47e04aabeda7c15b2d5cf9ae1f6
Kcsals <kcsals@gmail.com>
2023-08-27 13:02:17 -0600

    Added media settings and updated models

--- games/models.py
+++ games/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Game(models.Model):
     GENRE_CHOICES = [
         ('ACTION', 'Action'),
@@ -8,18 +7,48 @@ class Game(models.Model):
         ('STRATEGY', 'Strategy'),
         ('SPORTS', 'Sports'),
         ('ADVENTURE', 'Adventure'),
+        ('INDY', 'Indy'),
+        ('SANDBOX', 'Sandbox'),
+        ('FPS', 'Shooter'),
+        ('MOBA', 'MOBA')
         # Add other genres as needed
     ]
-
+        
     title = models.CharField(max_length=200)
-    description = models.TextField(help_text="Small description for the article link")
-    content = models.TextField(help_text="Main content of the article.")
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
-    game = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='articles')
     release_date = models.DateField()
     developer = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
     genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    rating = models.DecimalField(max_digits=3, decimal_places=1)  # Rating out of 10 with one decimal place
+    cover_image = models.ImageField(upload_to='game_covers/')  # Image field added
 
     def __str__(self):
         return self.title
+
+class ArticleBase(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    featured_image = models.ImageField(upload_to='article_images/')  # Image field added
+    # Any other common fields among all article types
+
+    class Meta:
+        abstract = True  # Makes sure this model won't be used to create any database table
+
+class Review(ArticleBase):
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    # Any other specific fields for reviews
+
+class News(ArticleBase):
+    # Specific fields for news, e.g. 'announcement_date'
+    pass
+
+class Guide(ArticleBase):
+    # Specific fields for tips and tricks if any
+    pass
+
+class Opinion(ArticleBase):
+    author_opinion = models.TextField()
+    # Any other specific fields for opinions
+
+
